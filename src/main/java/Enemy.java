62830bb49c40f143a2c0522fac3b3baac73ebe58
Francesco <med.dama@gmail.com>
2020-06-04 17:15:37 +0100

    Create Enemy and Player

---
+++ src/main/java/Enemy.java
@@ -0,0 +1,20 @@
+public abstract class Enemy {
+
+    private int health;
+
+    public Enemy(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public void takeDamage(int damageValue){
+        this.setHealth(this.getHealth() - damageValue);
+    }
+
+}
