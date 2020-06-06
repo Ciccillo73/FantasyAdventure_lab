6504c07ac6706aad975f8187c8b59e156c506f9d
Francesco <med.dama@gmail.com>
2020-06-06 14:12:25 +0100

    Create Dwarf

---
+++ src/test/java/players/fighters/DwarfTest.java
@@ -0,0 +1,44 @@
+package players.fighters;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DwarfTest {
+
+    @Test
+    public void setWeapon() {
+    }
+
+    @Test
+    public void getWeapon() {
+    }
+
+    @Test
+    public void attack() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getHealth() {
+    }
+
+    @Test
+    public void setHealth() {
+    }
+
+    @Test
+    public void getBlock() {
+    }
+
+    @Test
+    public void setBlock() {
+    }
+
+    @Test
+    public void blockAttack() {
+    }
+}
